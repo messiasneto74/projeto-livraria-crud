082fc9ae816bba950da34994f29fadca4eaa56a6
messiasneto74 <netomessias74@gmail.com>
2026-01-16 14:47:15 -0300

    ajustes resend

--- server/util/sendEmail.js
+++ server/util/sendEmail.js
@@ -1,16 +1,10 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"Livraria 📚" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
     to,
     subject,
     html,
