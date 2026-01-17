998809c6168ac60613a19661d03da80ee2bd31bd
messiasneto74 <netomessias74@gmail.com>
2026-01-16 23:51:59 -0300

    ajustes Routes

--- server/routes/api/authRoute.js
+++ server/routes/api/authRoute.js
@@ -4,13 +4,6 @@ const router = require("express").Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.get("/", (req, res) => {
-    const captcha = generateCaptcha();
-    req.session.captchaAnswer = captcha.answer;
 
-    res.json({
-        question: captcha.question,
-    });
-});
 module.exports = router;
 
